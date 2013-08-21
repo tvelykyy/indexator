afbb532f970ef024712ed1ff5093e7fcb58afe67
Taras Velykyy <tvelykyy@llnw.com>
2013-08-21 19:14:41 +0300

    unindexed_in_days column added.

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,24 +1,43 @@
-CREATE DATABASE `indexator` /*!40100 DEFAULT CHARACTER SET utf8 */$$
+/*
+Navicat MySQL Data Transfer
 
-delimiter $$
+Source Server         : localhost
+Source Server Version : 50532
+Source Host           : localhost:3306
+Source Database       : indexator
 
+Target Server Type    : MYSQL
+Target Server Version : 50532
+File Encoding         : 65001
+
+Date: 2013-08-21 19:13:26
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for page
+-- ----------------------------
+DROP TABLE IF EXISTS `page`;
 CREATE TABLE `page` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `url` varchar(250) NOT NULL,
   `title` varchar(300) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `id_UNIQUE` (`id`),
   UNIQUE KEY `url_UNIQUE` (`url`),
   KEY `fk_page_id` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8$$
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8;
 
-
-delimiter $$
-
+-- ----------------------------
+-- Table structure for unindexed_page
+-- ----------------------------
+DROP TABLE IF EXISTS `unindexed_page`;
 CREATE TABLE `unindexed_page` (
   `page_id` int(11) NOT NULL,
+  `unindexed_in_days` int(11) unsigned NOT NULL,
   PRIMARY KEY (`page_id`),
   UNIQUE KEY `page_id_UNIQUE` (`page_id`),
   KEY `fk_page_id` (`page_id`),
   CONSTRAINT `fk_page_id` FOREIGN KEY (`page_id`) REFERENCES `page` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8$$
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
