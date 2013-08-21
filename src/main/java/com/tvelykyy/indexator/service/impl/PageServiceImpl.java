b5b95b2ff1f9616d488314c7fbac4052a2d7ec7d
Taras Velykyy <tavelyky@gmail.com>
2013-08-22 00:48:11 +0300

    Transactions added.

--- src/main/java/com/tvelykyy/indexator/service/impl/PageServiceImpl.java
+++ src/main/java/com/tvelykyy/indexator/service/impl/PageServiceImpl.java
@@ -1,6 +1,27 @@
 package com.tvelykyy.indexator.service.impl;
 
+import com.tvelykyy.indexator.dao.PageDao;
+import com.tvelykyy.indexator.model.UnindexedPage;
 import com.tvelykyy.indexator.service.PageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+@Service
 public class PageServiceImpl implements PageService {
+
+    @Autowired
+    PageDao pageDao;
+
+    @Override
+    @Transactional
+    public void createNotIndexedPage(UnindexedPage unindexedPage) {
+        UnindexedPage page = new UnindexedPage();
+        page.setTitle("Title");
+        page.setUrl("http://google.com" + Math.random());
+        page.setUnindexedInDays(15);
+
+        pageDao.insertPage(page);
+        pageDao.insertUnindexedPage(page);
+    }
 }
